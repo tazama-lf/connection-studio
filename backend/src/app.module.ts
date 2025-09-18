d4e752ab47475dcc02bb20ef2b734ce7493e21d2
Muhammad Ali <ali.shoaib@paysyslabs.com>
2025-09-18 14:43:21 +0500

    feat: Knex Configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './config/env.validation';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env',
+    isGlobal: true,
+    validate,
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ backend/src/config/env.validation.ts
@@ -0,0 +1,51 @@
+import { plainToClass } from 'class-transformer';
+import {
+    IsEnum,
+    IsString,
+    IsNumberString,
+    validateSync,
+} from 'class-validator';
+
+enum NodeEnv {
+    DEVELOPMENT = 'dev',
+    PRODUCTION = 'prod',
+    TEST = 'test',
+}
+
+class EnvironmentVariables {
+    @IsEnum(NodeEnv)
+    NODE_ENV: NodeEnv;
+
+    @IsNumberString()
+    MAX_CPU: string;
+
+    @IsString()
+    FUNCTION_NAME: string;
+
+    @IsString()
+    CONFIGURATION_DATABASE_URL: string;
+    
+    @IsString()
+    CONFIGURATION_DATABASE: string;
+
+    @IsString()
+    CONFIGURATION_DATABASE_USER: string;
+    
+    @IsString()
+    CONFIGURATION_DATABASE_PASSWORD: string;
+}
+
+export const validate = (config: Record<string, unknown>) => {
+    const validatedConfig = plainToClass(EnvironmentVariables, config, {
+        enableImplicitConversion: true,
+    });
+    const errors = validateSync(validatedConfig, {
+        skipMissingProperties: false,
+    });
+
+    if (errors.length > 0) {
+        throw new Error(errors.toString());
+    }
+
+    return validatedConfig;
+};
