64d352af08a949f5f6fabdda41a99c97c0dd86a9
fatimali03 <fatima.ali@paysyslabs.com>
2025-10-06 12:46:18 +0500

    fix: remove unnecessary code after table changes

--- backend/src/data-model-extensions/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './data-model-extension.entity';
-export * from './data-model-extension.dto';
-export * from './data-model-extension.service';
-export * from './data-model-extension.repository';
-export * from './data-model-extension.controller';
-export * from './data-model-extension.module';
--- backend/src/mappings/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './mapping.module';
-export * from './multi-field-mapping.service';
-export * from './multi-field-mapping.controller';
-export * from './multi-field-mappings.repository';
-export * from './tazama-data-model.service';
-export * from './destination-field-extensions.repository';
