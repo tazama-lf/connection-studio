135e991a2844067ef35f4490d21bee35ca4f032b
Muhammad Ali <ali.shoaib@paysyslabs.com>
2025-10-27 18:46:33 +0500

    feat: implement Notify module and service for handling notifications

---
+++ backend/src/notify/notify.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { NotifyService } from './notify.service';
+import { LoggerModule } from 'src/logger-service/logger-service.module';
+
+@Module({
+  providers: [NotifyService],
+  imports: [LoggerModule]
+})
+export class NotifyModule { }
