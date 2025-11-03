1f3751daa634a4c129fd5a1dc274ba27b03167a3
Muhammad Ali <ali.shoaib@paysyslabs.com>
2025-11-03 19:38:12 +0500

    refactor: update SftpController to improve claim requirements and formatting

--- backend/src/sftp/sftp.controller.ts
+++ backend/src/sftp/sftp.controller.ts
@@ -10,7 +10,7 @@ import { ConfigService } from '@nestjs/config';
 import { SftpService } from './sftp.service';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { FetchSftpDto } from './dto/fetch-sftp.dto';
-import { RequireClaims, TazamaClaims } from 'src/auth/auth.decorator';
+import { RequireAnyClaims, RequireClaims, TazamaClaims } from 'src/auth/auth.decorator';
 import { TazamaAuthGuard } from 'src/auth/tazama-auth.guard';
 import { User } from 'src/auth/user.decorator';
 import { type AuthenticatedUser } from 'src/auth/auth.types';
@@ -27,14 +27,14 @@ export class SftpController {
   @Get('/all')
   @Serialize(FetchSftpDto)
   @RequireClaims(TazamaClaims.PUBLISHER)
-  async getFiles(@Query('format') format: 'de' | 'cron'| 'dems', @User() user: AuthenticatedUser) {
+  async getFiles(@Query('format') format: 'de' | 'cron' | 'dems', @User() user: AuthenticatedUser) {
     const sftpHost = this.configService.get<string>('SFTP_HOST_PRODUCER');
 
     if (!sftpHost) {
       throw new BadRequestException(`Producer SFTP server credentials not provided.`);
     }
 
-    return await this.sftpService.listFiles('/upload', format,user.tenantId);
+    return await this.sftpService.listFiles('/upload', format, user.tenantId);
   }
 
   @Get('/read')
@@ -43,7 +43,7 @@ export class SftpController {
     return await this.sftpService.readFile(name)
   }
   @Post('/publish')
-  @RequireClaims(TazamaClaims.PUBLISHER)
+  @RequireAnyClaims(TazamaClaims.PUBLISHER)
   async publishFile(@Query('name') name: string) {
     const sftpHost = this.configService.get<string>('SFTP_HOST_PRODUCER');
     if (!sftpHost) {
