8b23db159c6c8f9d7346f79aa8ce59f3050ae7aa
Syed Hassan Rizwan <hassan.rizwan@paysyslabs.com>
2026-01-06 19:25:59 +0500

    Corrected allowedhost to tcs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
-    allowedHosts: ['mm.paysyslabs.com'],
+    allowedHosts: ['tcs.paysyslabs.com'],
   },
   resolve: {
     alias: {
