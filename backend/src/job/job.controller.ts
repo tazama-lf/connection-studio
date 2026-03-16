d5a1eadeeb3a8a7156747c2cf558168b5389ec33
fatimali03 <fatima.ali@paysyslabs.com>
2026-03-16 10:59:35 +0500

    fix; added approver claim

--- backend/src/job/job.controller.ts
+++ backend/src/job/job.controller.ts
@@ -159,7 +159,7 @@ export class JobController {
   }
 
   @Patch('/update/activation/:id')
-  @RequireAnyClaims(TazamaClaims.PUBLISHER)
+  @RequireAnyClaims(TazamaClaims.PUBLISHER, TazamaClaims.APPROVER)
   @Audit()
   async updateJobActivation(
     @Param('id') id: string,
