ce1bf282b4962451e2d4db17f387b567fc648e81
fatimali03 <fatima.ali@paysyslabs.com>
2026-04-01 17:00:33 +0500

    fix: routes

--- backend/src/constants/constant.ts
+++ backend/src/constants/constant.ts
@@ -2,7 +2,7 @@ export const DEV_BASE_URL = 'http://localhost:3100';
 export const UAT_BASE_URL = 'http://localhost:3100';
 export const CONFIG_URL = '/v1/admin/tcs/config';
 export const CONFIG_WRITE_URL = '/v1/admin/tcs/config/write';
-export const CONFIG_STATUS_URL = '/v1/admin/tcs/tcs/config/status';
+export const CONFIG_STATUS_URL = '/v1/admin/tcs/config/status';
 export const DATA_MODEL_COLLECTIONS_URL =
   '/v1/admin/tcs/data-model/collections';
 export const DESTINATION_TYPES_URL =
