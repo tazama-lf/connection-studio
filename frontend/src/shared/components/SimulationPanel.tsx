17aaf06252aa50de558f52b653fb549c72c1d62c
Muhammad Ali <ali.shoaib@paysyslabs.com>
2026-06-19 11:10:05 +0500

    feat: xml attributes allowed in parsing
    Signed-off-by: Muhammad Ali <ali.shoaib@paysyslabs.com>

--- frontend/src/shared/components/SimulationPanel.tsx
+++ frontend/src/shared/components/SimulationPanel.tsx
@@ -92,7 +92,7 @@ export const SimulationPanel: React.FC<SimulationPanelProps> = ({
       const payloadType = contentType === 'application/json' ? 'json' : 'xml';
 
       const xmlparser = new XMLParser({
-        ignoreAttributes: true,
+        ignoreAttributes: false,
         attributeNamePrefix: '',
       });
       const jsonResult = xmlparser.parse(testPayload);
